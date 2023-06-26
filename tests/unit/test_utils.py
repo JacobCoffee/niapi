695666550d9b074396b5e443bacece7da66da116
Jacob Coffee <jcoffee5@oreillyauto.com>
2023-06-25 23:57:15 -0500

    fix(tests): resolves issue with `mypy` (read: me) being stupid

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -9,12 +9,12 @@ from hypothesis import given
 from niapi.utils import camel_case, case_insensitive_string_compare, check_email, dataclass_as_dict_shallow, slugify
 
 
-@given(st.emails())  # type: ignore[misc]
+@given(st.emails())
 def test_check_email(email: str) -> None:
     assert check_email(email) == email.lower()
 
 
-@given(st.text(alphabet=string.ascii_letters + string.digits + string.whitespace + "-"))  # type: ignore[misc]
+@given(st.text(alphabet=string.ascii_letters + string.digits + string.whitespace + "-"))
 def test_slugify(value: str) -> None:
     if all(c in string.whitespace or c == "-" for c in value):
         assert slugify(value) == re.sub(r"[-\s]+", "-", value.lower()).strip("-_")
@@ -22,19 +22,19 @@ def test_slugify(value: str) -> None:
         assert slugify(value)
 
 
-@given(st.text(st.characters(blacklist_characters="_")))  # type: ignore[misc]
+@given(st.text(st.characters(blacklist_characters="_")))
 def test_camel_case(string: str) -> None:
     assert camel_case(string) == "".join(
         word if index == 0 else word.capitalize() for index, word in enumerate(string.split("_"))
     )
 
 
-@given(st.text(), st.text())  # type: ignore[misc]
+@given(st.text(), st.text())
 def test_case_insensitive_string_compare(a: str, b: str) -> None:
     assert case_insensitive_string_compare(a, b) == (a.strip().lower() == b.strip().lower())
 
 
-@given(st.text(min_size=1), st.integers())  # type: ignore[misc]
+@given(st.text(min_size=1), st.integers())
 def test_dataclass_as_dict_shallow(name: str, value: int) -> None:
     @dataclass
     class TestClass:
