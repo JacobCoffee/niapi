b42651aba5915d1d6e0361409ff99bc179a707fd
Alc-Alc <alc@localhost>
2023-10-11 22:04:26 +0530

    Enhancement: Utilize Jinja loops to clean up partial.html

--- app/domain/calculator/schema.py
+++ app/domain/calculator/schema.py
@@ -39,6 +39,7 @@ class NetworkInfo(BaseModel):
             ...,
             description="The subnet mask in standard IPv4 format.",
             pattern=r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+            serialization_alias="Subnet Mask",
         ),
     ]
     wildcard_subnet_mask: Annotated[
@@ -47,21 +48,48 @@ class NetworkInfo(BaseModel):
             ...,
             description="The wildcard subnet mask in standard IPv4 format.",
             pattern=r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+            serialization_alias="Wildcard Subnet Mask",
         ),
     ]
-    total_ips: Annotated[int, Field(..., description="The total number of IPs in the network.")]
-    usable_ips: Annotated[int, Field(..., description="The total number of usable IPs in the network.")]
+    total_ips: Annotated[
+        int, Field(..., description="The total number of IPs in the network.", serialization_alias="Total IPs")
+    ]
+    usable_ips: Annotated[
+        int, Field(..., description="The total number of usable IPs in the network.", serialization_alias="Usable IPs")
+    ]
     network_ip: Annotated[
         str,
-        Field(..., description="The network IP in standard IPv4 format.", pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"),
+        Field(
+            ...,
+            description="The network IP in standard IPv4 format.",
+            pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$",
+            serialization_alias="Network IP",
+        ),
     ]
     broadcast_ip: Annotated[
         str,
-        Field(..., description="The broadcast IP in standard IPv4 format.", pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"),
+        Field(
+            ...,
+            description="The broadcast IP in standard IPv4 format.",
+            pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$",
+            serialization_alias="Broadcast IP",
+        ),
     ]
     first_ip: Annotated[
-        str, Field(..., description="The first IP in standard IPv4 format.", pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+        str,
+        Field(
+            ...,
+            description="The first IP in standard IPv4 format.",
+            pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$",
+            serialization_alias="First IP",
+        ),
     ]
     last_ip: Annotated[
-        str, Field(..., description="The last IP in standard IPv4 format.", pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$")
+        str,
+        Field(
+            ...,
+            description="The last IP in standard IPv4 format.",
+            pattern=r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$",
+            serialization_alias="Last IP",
+        ),
     ]
